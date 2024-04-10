714f933c6875b8e74ff6a77889e172f2367d27cc
JacobCrabill <jacob.crabill@gmail.com>
2024-04-09 21:28:11 -0700

    Update zigdown dep; add to README and example deck
    Also add dap-config.lua for debugging with nvim-dap

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Slidey: Elegant Slideshows in Zig
 
 Use Markdown to create simple yet elegant slide shows
+
+Test example:
+
+```bash
+zig build -Doptimize=ReleaseSafe
+./zig-out/bin/slidey -d test/deck
+```
---
+++ dap-config.lua
@@ -0,0 +1,19 @@
+return {
+  configurations = {
+    zig = {
+      {
+        name = "Test Slide Deck",
+        type = "lldb",
+        request = "launch",
+        cwd = "${workspaceFolder}",
+        program = "${workspaceFolder}" .. "/zig-out/bin/slidey",
+        args = { "-d", "test/deck" },
+      },
+    },
+  },
+  adapters = {
+    -- List of adapter tables here
+    -- The name of each table becomes the 'type' used in the config
+    -- e.g.: lldb = { name = 'lldb', type = 'executable', command = '/path/to/lldb-vscode' }
+  },
+}
